0beac2b2ee03f5e4c7ae39469800c031278b8576
NRGetRekt <92243993+NRGetRekt@users.noreply.github.com>
2023-02-06 21:32:06 -0500

    main

---
+++ main.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const ytdl = require('ytdl-core');
+
+client.on('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('message', message => {
+    if (message.content.startsWith('!play')) {
+        const voiceChannel = message.member.voiceChannel;
+        if (!voiceChannel) {
+            return message.reply('Please be in a voice channel first!');
+        }
+        voiceChannel.join().then(connection => {
+            const stream = ytdl(message.content.split(' ')[1], { filter : 'audioonly' });
+            const dispatcher = connection.playStream(stream);
+            dispatcher.on('end', () => voiceChannel.leave());
+        });
+    }
+});
+
+client.login('YOUR_TOKEN_HERE');
