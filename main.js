8c12c8a2cb0b921e90854b1328b925f0fb418828
NRGetRekt <92243993+NRGetRekt@users.noreply.github.com>
2023-02-06 23:44:59 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, GatewayIntentBits } = require('discord.js');
 const ytdl = require('ytdl-core');
+const client = new Client({
+        intents: [
+                GatewayIntentBits.Guilds,
+                GatewayIntentBits.GuildMessages,
+                GatewayIntentBits.MessageContent,
+        ],
+});
 
 client.on('ready', () => {
     console.log('Ready!');
